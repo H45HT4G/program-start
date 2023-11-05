f92d55e789248865e4863b94f6650d904e29f3f6
Karolina Syka <syka.karolina@gmail.com>
2023-11-05 17:46:01 +0100

    first project for studies - Wyścigi kolarskie

---
+++ Wyscig/Wyscig/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wyscig
+{
+    internal class Program
+    {
+        //  stworzenie listy z zawodnikami
+
+        static List<Competitor> Competitors = new List<Competitor>();
+
+        static public int ID { get; set; }
+
+        //  stworzenie struktury 'competitor' przechowujaca dane o zawodniku
+
+        struct Competitor
+        {
+            public int id, ranking;
+            public string name, surName, nationality, email, sex;
+            public byte age;
+            public bool experience;
+        }
+
+        //  klasa glowna main zwracajaca void
+
+        static void Main(string[] args)
+        {
+            //  build interfejsu menu
+
+            int choice;
+
+            do
+            {
+
+                //  metoda odwolujaca sie do interfejsu uzytkownika
+
+                DisplayMenu();
+
+                //  petla zabezpieczajaca kod/program przed zlym wprowadzeniem danych/zmiennej
+
+                while ((!int.TryParse(Console.ReadLine(), out choice)) || (choice < 0 || choice > 5))
+                {
+                    Console.WriteLine("Niepoprawny wybór, wprowadź wybór ponownie - tylko cyfry od 0 do 5!");
+                }
+
+                // switch wybierajacy opcje z menu i jej callback
+
+                switch (choice)
+                {
+                    case 1:
+                        AddCompetitor();
+                        break;
+
+                    case 2:
+                        DelCompetitor(1);
+                        break;
+
+                    case 3:
+                        EditCompetitor(1);
+                        break;
+
+                    case 4:
+                        DisplayCompetitor(1);
+                        break;
+
+                    case 5:
+                        DisplayAll();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (choice != 0);
+            CloseProgram();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Aplikacja wyśćig pokoju menu:\n" +
+                "1. Dodawanie zawodnika\n" +
+                "2. Usuwanie zawodnika\n" +
+                "3. Edytowanie zawodnika\n" +
+                "4. Wyświetl zawodnika\n" +
+                "5. Wyświetla wszystkich zawodnikó\n" +
+                "0. Zamknij program");
+        }
+
+        //  klasy handlujace zawodnikow
+
+        static string Credentials(string credentialsToDisplay)
+        {
+            Console.Clear();
+            Console.WriteLine(credentialsToDisplay);
+            return Console.ReadLine();
+        }
+
+        static void AddCompetitor()
+        {
+            Competitor newCompetitor = new Competitor();
+            newCompetitor.id = ID;
+            newCompetitor.name = Credentials("Podaj imię: ");
+            newCompetitor.surName = Credentials("Podaj nazwisko: ");
+            newCompetitor.nationality = Credentials("Podaj nazwisko: ");
+            newCompetitor.age = byte.Parse(Credentials("Podaj wiek: "));
+            newCompetitor.email = Credentials("Podaj email: ");
+            newCompetitor.sex = Credentials("Podaj płeć(K/M): ").ToUpper();
+            newCompetitor.experience = Credentials("Czy jesteś zawodowcem?(T/N): ") ==  "T" ? true : false;
+            newCompetitor.ranking = int.Parse(Credentials("Podaj ranking: "));
+            Competitors.Add(newCompetitor);
+            ID++;
+        }
+
+        static void DelCompetitor(int _id)
+        {
+
+        }
+
+        static void EditCompetitor(int _id)
+        {
+
+        }
+
+        static void DisplayCompetitor(int _id)
+        {
+
+        }
+
+        static void DisplayAll()
+        {
+
+        }
+
+        static void CloseProgram()
+        {
+
+        }
+    }
+}
---
+++ Wyscig/Wyscig/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Wyscig")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wyscig")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("b90ea811-8e67-4c5f-9dc3-c42a0521b58b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
